8d3949df0d630b3d37626751075569f814031df8
Hanna Bennett <bennetthanna22@gmail.com>
2017-12-05 22:57:42 -0800

    Start wiring up fragment widgets

--- app/src/main/java/com/hannabennett/to_do/TaskFragment.java
+++ app/src/main/java/com/hannabennett/to_do/TaskFragment.java
@@ -2,18 +2,72 @@ package com.hannabennett.to_do;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
 /**
  * Created by HannaBennett on 12/5/17.
  */
 
 public class TaskFragment extends Fragment {
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mCompletedCheckBox;
+    private Task mTask;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_task, container, false);
+
+        mTitleField = (EditText) view.findViewById(R.id.task_title);
+        mTitleField.setText(mTask.getTitle());
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mTask.setTitle(s.toString());
+                updateCrime();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mDateButton = (Button) view.findViewById(R.id.task_due_date);
+        updateDate();
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment().newInstance(mTask.getDate());
+                dialog.setTargetFragment(TaskFragment.this, REQUEST_DATE);
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
+
+        mCompletedCheckBox = (CheckBox) view.findViewById(R.id.task_completed);
+        mCompletedCheckBox.setChecked(mTask.isCompleted());
+        mCompletedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mTask.setCompleted(isChecked);
+                updateCrime();
+            }
+        });
 
         return view;
     }
